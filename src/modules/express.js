1a1c41027df40a88e59ccfc37f440a8ee98f1775
isaquesv <isaque.s.venancio43@gmail.com>
2025-03-23 15:34:21 -0300

    Adicionando a validação de notícias com a IA Gemini

--- src/modules/express.js
+++ src/modules/express.js
@@ -2,7 +2,10 @@ import { createTableUsuario } from "../model/usuarioModel.js";
 // Criando as tabela
 await createTableUsuario();
 
+// Permite o uso de APIs e recursos externos. Sem o CORS tais recursos estariam bloqueados
 import express from "express";
+import cors from "cors";
+import multer from "multer";
 const app = express();
 app.use(express.json());
 
@@ -11,6 +14,22 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Middlewares
+// Permite o uso de APIs e recursos externos. Sem o CORS tais recursos estariam bloqueados
+app.use(cors());
+
+// Configurar Multer para armazenar o arquivo em memória
+const storage = multer.memoryStorage();
+const upload = multer({ 
+    storage: storage, 
+    // Limite de 50MB
+    limits: { fileSize: 50 * 1024 * 1024 }
+});
+
+// Permite interpretar requisições JSON, transformando-os em objetos JavaScript
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Servindo arquivos estáticos da pasta "public"
 app.use(express.static("public"));
@@ -69,5 +88,10 @@ app.get("/consultar-comentarios.html", (req, res) => {
 import usuarioRoutes from "../routes/usuarioRoutes.js";
 // Definindo as rotas para funções atreladas ao usuário (como cadastro, login, etc.)
 app.use("/usuario", usuarioRoutes);
+
+// Importando as rotas relacionadas a publicação
+import publicacaoRoutes from "../routes/publicacaoRoutes.js";
+// Definindo as rotas para funções atreladas a publicacao (como validação de descrição, imagens, etc.)
+app.use("/publicacao", publicacaoRoutes);
 
 export default app;
