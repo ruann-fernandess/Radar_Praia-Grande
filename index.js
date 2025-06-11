1900571d7506b5139efb969c4d4b059089bff8e5
Isaque Silva Venancio <166270471+isaquesv@users.noreply.github.com>
2025-06-10 22:28:51 -0300

    Atualizando o index.js
    Passando o sistema de HTTPS para HTTP

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import dotenv from "dotenv";
 import app from "./src/modules/express.js";
-import https from "https";
-import fs from 'fs';
-import chalk from 'chalk';
 
 dotenv.config();
 
 const port = process.env.PORT;
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt')
-  }, app).listen(port, () => {
-     console.log(chalk.magentaBright.bold(`Disponível em: https://localhost:8080/login.html (pode ser necessário autorizar entrada)`));
-  })
+app.listen(port, () => {
+    console.log(`Rodando com express na porta ${port}!`);
+});
